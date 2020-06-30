77f9afe9092572ceb6cc16aa5a4618c96f6f1bad
jiangnan <yeah_20@163.com>
2020-07-01 00:41:36 +0800

    add three components

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,27 +1,32 @@
-import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter, {RouteConfig} from 'vue-router';
+import Bills from '@/views/Bills.vue';
+import Labels from '@/views/Labels.vue';
+import Statistics from '@/views/Statistics.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-  const routes: Array<RouteConfig> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+const routes: Array<RouteConfig> = [
+    {
+        path: '/',
+        redirect: '/bills'
+    },
+    {
+        path: '/bills',
+        component: Bills
+    },
+    {
+        path: '/labels',
+        component: Labels
+    },
+    {
+        path: '/statistics',
+        component: Statistics
+    }
+];
 
 const router = new VueRouter({
-  routes
-})
+    routes
+});
 
-export default router
+export default router;
---
+++ src/views/Bills.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>Bills.vue</div>
+</template>
+
+<script lang="ts">
+    export default {
+        name: 'Bills'
+    };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
