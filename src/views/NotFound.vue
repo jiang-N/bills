4b7f9c28d543a8d8a7c69d5fd710902ef0fdaaa8
jiangnan <yeah_20@163.com>
2020-07-02 22:36:25 +0800

    添加404页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <div>您访问的页面不存在，请检查路径</div>
+        <router-link to="/">返回首页</router-link>
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        name: 'NotFound'
+    };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
