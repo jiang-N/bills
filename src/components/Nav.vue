955a8ffa4242a3f4b1e2445754a92b6c58884db8
jiangnan <yeah_20@163.com>
2020-07-06 23:19:00 +0800

    全局<Icon/>组件

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,10 +1,19 @@
 <template>
     <div class="nav">
-        <router-link to="/bills">记账</router-link>
+        <router-link to="/bills">
+            <Icon name="bills"></Icon>
+            记账
+        </router-link>
         |
-        <router-link to="/labels">标签</router-link>
+        <router-link to="/labels">
+            <Icon name="labels"></Icon>
+            标签
+        </router-link>
         |
-        <router-link to="/statistics">统计</router-link>
+        <router-link to="/statistics">
+            <Icon name="statistics"></Icon>
+            统计
+        </router-link>
     </div>
 
 </template>
@@ -16,7 +25,7 @@
 </script>
 
 <style lang="scss" scoped>
-    .nav{
+    .nav {
         border: 1px solid red;
     }
 </style>
