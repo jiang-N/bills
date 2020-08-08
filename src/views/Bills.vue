1764a1d8d4dd52cb7dc073664774328c3fb8537a
jiangnan <yeah_20@163.com>
2020-08-08 14:45:14 +0800

    使用全局 tagList

--- src/views/Bills.vue
+++ src/views/Bills.vue
@@ -20,13 +20,12 @@
     import tagListModel from '@/models/tagListModel';
 
     const recordList = recordListModel.fetch();
-    const tagList = tagListModel.fetch();
 
     @Component({
         components: {Tags, FormItem, Types, NumberPad}
     })
     export default class Bills extends Vue {
-        tags = tagList;
+        tags = window.tagList;
         recordList: RecordItem[] = recordList;
         record: RecordItem = {
             tags: [], notes: '', type: '-', amount: 0
