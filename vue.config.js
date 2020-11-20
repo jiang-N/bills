fb2c4242dc9d7e0395cd04f785f2a54b8ec7ffa1
jiangnan <yeah_20@163.com>
2020-11-20 21:36:53 +0800

    部署网站到 bills-website

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,20 @@
-const path = require("path")
+const path = require('path')
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/bills-website/'
+        : '/',
     lintOnSave: false,
     chainWebpack: config => {
-        const dir = path.resolve(__dirname, "src/assets/icons")
+        const dir = path.resolve(__dirname, 'src/assets/icons')
 
         config.module
-            .rule("svg-sprite")
+            .rule('svg-sprite')
             .test(/\.svg$/)
             .include.add(dir).end()
-            .use("svg-sprite-loader").loader("svg-sprite-loader").options({extract: false}).end()
-            .use("svgo-loader").loader("svgo-loader")
-            .tap(options => ({...options, plugins: [{removeAttrs: {attrs: "fill"}}]})).end()
-        config.plugin("svg-sprite").use(require("svg-sprite-loader/plugin"), [{plainSprite: true}])
-        config.module.rule("svg").exclude.add(dir)
+            .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract: false}).end()
+            .use('svgo-loader').loader('svgo-loader')
+            .tap(options => ({...options, plugins: [{removeAttrs: {attrs: 'fill'}}]})).end()
+        config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSprite: true}])
+        config.module.rule('svg').exclude.add(dir)
     }
 }
