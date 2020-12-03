1812c727de24e9ed2c0301f329862675e392f50d
jiang-N <yeah_20@163.com>
2020-12-03 11:17:45 +0800

    添加 createdAt 输入框

--- src/views/Bills.vue
+++ src/views/Bills.vue
@@ -1,66 +1,69 @@
 <template>
-    <Layout class-prefix="layout">
-        <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
-        <Tabs :data-source="recordTypeList" :value.sync="record.type"/>
-        <div class="notes">
-            <FormItem field-name="备注" placeholder="在这里输入备注" :value.sync="record.notes"/>
-        </div>
-        <Tags @update:value="record.tags=$event"/>
-    </Layout>
+  <Layout class-prefix="layout">
+    <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
+    <Tabs :data-source="recordTypeList" :value.sync="record.type"/>
+    <div class="createdAt">
+      <FormItem type="date" field-name="日期" placeholder="在这里输入日期" :value.sync="record.createdAt"/>
+    </div>
+    <div class="notes">
+      <FormItem field-name="备注" placeholder="在这里输入备注" :value.sync="record.notes"/>
+    </div>
+    <Tags @update:value="record.tags=$event"/>
+  </Layout>
 </template>
 
 <script lang="ts">
-    import Vue from 'vue';
-    import {Component} from 'vue-property-decorator';
-    import NumberPad from '@/components/Bills/NumberPad.vue';
-    import FormItem from '@/components/Bills/FormItem.vue';
-    import Tags from '@/components/Bills/Tags.vue';
-    import Tabs from '@/components/Tabs.vue';
-    import recordTypeList from '@/constants/recordTypeList';
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+import NumberPad from '@/components/Bills/NumberPad.vue';
+import FormItem from '@/components/Bills/FormItem.vue';
+import Tags from '@/components/Bills/Tags.vue';
+import Tabs from '@/components/Tabs.vue';
+import recordTypeList from '@/constants/recordTypeList';
 
-    @Component({
-        components: {Tabs, Tags, FormItem, NumberPad}
-    })
-    export default class Bills extends Vue {
-        get recordList() {
-            return this.$store.state.recordList;
-        }
+@Component({
+  components: {Tabs, Tags, FormItem, NumberPad}
+})
+export default class Bills extends Vue {
+  get recordList() {
+    return this.$store.state.recordList;
+  }
 
-        recordTypeList = recordTypeList;
-        record: RecordItem = {
-            tags: [], notes: '', type: '-', amount: 0
-        };
+  recordTypeList = recordTypeList;
+  record: RecordItem = {
+    tags: [], notes: '', type: '-', amount: 0, createdAt: new Date().toISOString()
+  };
 
-        created() {
-            this.$store.commit('fetchRecords');
-        }
+  created() {
+    this.$store.commit('fetchRecords');
+  }
 
-        onUpdateNotes(value: string) {
-            this.record.notes = value;
-        }
+  onUpdateNotes(value: string) {
+    this.record.notes = value;
+  }
 
-        saveRecord() {
-            if (!this.record.tags.length || this.record.tags.length === 0) {
-                return window.alert('请至少选择一个标签');
-            }
-            this.$store.commit('createRecord', this.record);
-            if (this.$store.state.createRecordError === null) {
-                window.alert('已保存');
-                this.record.notes = '';
-            }
-        }
+  saveRecord() {
+    if (!this.record.tags.length || this.record.tags.length === 0) {
+      return window.alert('请至少选择一个标签');
     }
+    this.$store.commit('createRecord', this.record);
+    if (this.$store.state.createRecordError === null) {
+      window.alert('已保存');
+      this.record.notes = '';
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
-    ::v-deep .layout-content {
-        display: flex;
-        flex-direction: column-reverse;
+::v-deep .layout-content {
+  display: flex;
+  flex-direction: column-reverse;
 
-        > .notes {
-            padding: 12px 0;
-        }
-    }
+  > .notes {
+    padding: 12px 0;
+  }
+}
 
 </style>
 
